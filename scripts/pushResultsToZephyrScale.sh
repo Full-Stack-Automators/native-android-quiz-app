6150f60a4ff42f37531f34dfcb32973a59abb1e0
Jace Parara <jace.parara@fullstackautomators.com>
2023-09-07 10:10:10 +0100

    Clean up Zephyr scale bash file

--- scripts/pushResultsToZephyrScale.sh
+++ scripts/pushResultsToZephyrScale.sh
@@ -2,14 +2,14 @@
 #  Zephyr base URL.
 #  DON'T CHANGE THE CONSTANT BELOW. KEEP IT AS IT IS.
 ######################################################################
-zephyrBaseUrl="https://api.zephyrscale.smartbear.com/v2/automations/executions/junit?projectKey=MA&autoCreateTestCases=true"
+zephyrBaseUrl="https://api.zephyrscale.smartbear.com/v2/automations/executions"
 
 #######################################################################
 #  Access and secret keys, and user id needed for connection to Zephyr for Jira.
 #  Replace the constants below with values relevant to your project and account.
 #######################################################################
 
-# The accessKey and secretKey to access your project. You can find them in your Jira project: Zephyr > API Keys.
+# The accessKey and secretKey to access your project. You can find them in your Jira Account settings: Profile picture > Zephyr Scale API Access Token > Create Access Token.
 bearerToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOiJodHRwczovL2Z1bGxzdGFja2F1dG9tYXRvcnMuYXRsYXNzaWFuLm5ldCIsInVzZXIiOnsiYWNjb3VudElkIjoiNzEyMDIwOjhkNjE5ZWM2LTNhNjUtNDk4ZS04YmM2LTIwY2I4ODMwYmUzYiJ9fSwiaXNzIjoiY29tLmthbm9haC50ZXN0LW1hbmFnZXIiLCJzdWIiOiI5MjUyZWYwMC03NzQ4LTNlYzctODcyYi04MjNjYzFhY2Q2MzAiLCJleHAiOjE3MjUxMTA2NzYsImlhdCI6MTY5MzU3NDY3Nn0.XZ0YRGEmFcY4R94hfsr8owDJISoRK7i6g4RdtSEH7Fc"
 
 #######################################################################
@@ -17,6 +17,7 @@ bearerToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb250ZXh0Ijp7ImJhc2VVcmwiOi
 #  Replace the values below with data relevant to your project.
 #######################################################################
 
+projectKey="MA"
 xmlPath="@//Users/Jace/repo/native-android-quiz-app/app/build/outputs/androidTest-results/connected/TEST-Copy_of_Pixel_6_API_31(AVD) - 12-app-.xml"
 autoCreateTestCases="true"
 
@@ -24,6 +25,6 @@ autoCreateTestCases="true"
 #  Create an automation task, run it, send test results to Zephyr.
 #  Keep this section as it is.
 #######################################################################
-echo "Uploading test results to Zephyr ..."
-curl -H "Authorization: Bearer $bearerToken" -F "file=$xmlPath;type=application/xml" "$zephyrBaseUrl"
-echo "Test results uploaded successfully"
+echo "Uploading test results to Zephyr Scale..."
+curl -H "Authorization: Bearer $bearerToken" -F "file=$xmlPath;type=application/xml" "$zephyrBaseUrl/junit?projectKey=$projectKey&autoCreateTestCases=$autoCreateTestCases"
+echo "\nTest results uploaded successfully"
